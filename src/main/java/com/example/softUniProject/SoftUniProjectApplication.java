15c1f8ffad5372b396542c65cde77cbdb2baf7a2
SoftUniProject <maikimn@abv.bg>
2023-11-17 00:57:03 +0200

    First commit

---
+++ src/main/java/com/example/softUniProject/SoftUniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.softUniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftUniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftUniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/softUniProject/SoftUniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softUniProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftUniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
